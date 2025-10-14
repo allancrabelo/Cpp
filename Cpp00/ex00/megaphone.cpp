e91baef3f58b1a3d128bc33706285d7048406ce6
allancrabelo <allanaugustocarneirorabelo@gmail.com>
2025-10-14 18:35:38 +0100

    Initial Setup of Megaphone Exercise Added a setup: proto program and Makefile

---
+++ Cpp00/ex00/megaphone.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+		return (cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl, 0);
+	return (0);
+}
