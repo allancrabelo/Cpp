20047c3ab1915298aab3778f45aab39e990f97c5
allancrabelo <allanaugustocarneirorabelo@gmail.com>
2025-11-12 07:41:55 +0000

    Megaphone finished Rignt now, megaphone working properly. Modifed to working only with one space and with new line on string end.

--- Cpp00/ex00/megaphone.cpp
+++ Cpp00/ex00/megaphone.cpp
@@ -1,22 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaugusto <aaugusto@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/12 07:32:52 by aaugusto          #+#    #+#             */
+/*   Updated: 2025/11/12 07:35:56 by aaugusto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 #include <cctype>
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	if (argc == 1)
-		return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl, 0);
-	else
 	{
-		for (int i = 1; i < argc; i++)
-		{
-			for (int j = 0; argv[i][j]; j++)
-			{
-				std::cout << (char)toupper(argv[i][j]);
-			}
-			if (i < argc - 1)
-				std::cout << " ";
-		}
-		std::cout << "\n";
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
 	}
-	return (0);
+	bool first = true;
+	for (int i = 1; i < argc; i++)
+	{
+		char	*start = argv[i];
+		char	*end = argv[i];
+
+		while (*end)
+			end++;
+		while (end > start && std::isspace(*(end - 1)))
+			end--;
+		while (std::isspace(*start) && start < end)
+			start++;
+		if (start < end)
+		{
+			if (!first)
+				std::cout << " ";
+			first = false;
+			for (char *p = start; p < end; p++)
+				std::cout << char(std::toupper(*p));
+		}
+	}
+	std::cout << std::endl;
+	return 0;
 }
