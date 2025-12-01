a1929b1f31be53cac0c14372c161b002b96dc336
allancrabelo <allanaugustocarneirorabelo@gmail.com>
2025-12-01 10:30:28 +0000

    New Megaphone Pipeline

--- Cpp00/ex00/megaphone.cpp
+++ Cpp00/ex00/megaphone.cpp
@@ -6,41 +6,41 @@
 /*   By: aaugusto <aaugusto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 07:32:52 by aaugusto          #+#    #+#             */
-/*   Updated: 2025/11/12 08:06:18 by aaugusto         ###   ########.fr       */
+/*   Updated: 2025/12/01 10:28:23 by aaugusto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cctype>
+#include <iostream>
+#include <cctype>
+#include <string>
+#include <cstdlib>
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	if (argc == 1)
-	{
-		std::cerr << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return (EXIT_FAILURE);
-	}
-	bool first = true;
+	bool printed = false;
+
 	for (int i = 1; i < argc; i++)
 	{
-		char	*start = argv[i];
-		char	*end = argv[i];
-
-		while (*end)
-			end++;
-		while (end > start && std::isspace(*(end - 1)))
-			end--;
-		while (std::isspace(*start) && start < end)
-			start++;
-		if (start < end)
-		{
-			if (!first)
-				std::cout << " ";
-			first = false;
-			for (char *p = start; p < end; p++)
-				std::cout << char(std::toupper(*p));
-		}
+		std::string arg(argv[i]);
+		size_t start = arg.find_first_not_of(" \t");
+		size_t end = arg.find_last_not_of(" \t");
+		if (start == std::string::npos)
+			continue;
+		arg = arg.substr(start, end - start + 1);
+		for (size_t j = 0; j < arg.length(); j++)
+			arg[j] = std::toupper(arg[j]);
+		if (printed)
+			std::cout << " ";
+		std::cout << arg;
+		printed = true;
 	}
+	if (!printed)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	std::cout << std::endl;
 	return (EXIT_SUCCESS);
 }
+
+
+
