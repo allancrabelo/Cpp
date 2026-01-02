3c75a115ed9f5f08e68c428dce352e4df2070bf3
allancrabelo <allanaugustocarneirorabelo@gmail.com>
2026-01-02 18:17:17 +0000

    added: new cpp directory.

---
+++ Cpp00/ex01/ex00/megaphone.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                              _             */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaugusto <aaugusto@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/12 07:32:52 by aaugusto          #+#    #+#             */
+/*   Updated: 2025/12/27 16:56:15 by aaugusto                                 */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+#include <iostream>
+#include <cctype>
+#include <string>
+#include <cstdlib>
+
+int main(int argc, char **argv)
+{
+	bool printed = false;
+
+	for (int i = 1; i < argc; i++)
+	{
+		std::string arg(argv[i]);
+		size_t start = arg.find_first_not_of(" \t");
+		size_t end = arg.find_last_not_of(" \t");
+		if (start == std::string::npos)
+			continue;
+		arg = arg.substr(start, end - start + 1);
+		for (size_t j = 0; j < arg.length(); j++)
+			arg[j] = std::toupper(arg[j]);
+		if (printed)
+			std::cout << " ";
+		std::cout << arg;
+		printed = true;
+	}
+	if (!printed)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << std::endl;
+	return (EXIT_SUCCESS);
+}
+
+
+
---
+++ Cpp02/ex00/Fixed.hpp
@@ -0,0 +1,16 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+class	Fixed
+{
+	private:
+		int	_value;
+		static const int _fractional = 8;
+	public:
+		Fixed(void);
+	
+		~Fixed(void);
+		
+};
+
+#endif
