bd84bb9a13d289cfa57621807236688fcb92b6c7
allancrabelo <allanaugustocarneirorabelo@gmail.com>
2026-04-03 08:56:51 +0100

    added: new files. ex00 new files added

--- Cpp05/ex00/Bureaucrat.hpp
+++ Cpp05/ex00/Bureaucrat.hpp
@@ -17,5 +17,35 @@ class		Bureaucrat
 		Bureaucrat &operator=(const Bureaucrat &copy); // Copy Assignment Constructor
 		~Bureaucrat(); // Destructor
 
-		
+	// [ Expeptions ]:
+	class	GradeTooHighException : public	std::exeption
+	{
+		public:
+			virtual const char* what() const throw()
+			{
+				return "Grade is too high!";
+			}	
+	};
+
+	class	GradeTooLowException : public std::exeption
+	{
+		public:
+			virtual	const char* what() const throw()
+			{
+				return "Grade is too low!";
+			}	
+	};
+
+	// [ Getters ]:
+	const std::string	getName() const;
+	int					getGrade() const;
+
+	// [ Mutators ]:
+	void				increment();
+	void				decrement();
+	
 };
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &bureaucrat);
+
+#endif
